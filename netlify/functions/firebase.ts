ea2b731345677a1c396c5b9e7c1cdc3f932ffe25
adrian1520 <adrian.adamowicz.x@gmail.com>
2026-04-22 10:28:40 +0200

    Update firebase.ts

--- netlify/functions/firebase.ts
+++ netlify/functions/firebase.ts
@@ -1,1 +1,22 @@
-export const x=1;
+import { initializeApp, cert, getApps } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+
+let db;
+
+export function getDB() {
+  if (!getApps().length) {
+    initializeApp({
+      credential: cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      }),
+    });
+  }
+
+  if (!db) {
+    db = getFirestore();
+  }
+
+  return db;
+}
